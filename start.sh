2e2c92610057ec65c86ad2d019649f3d30b143bf
MAJORTG <99739229+MAJORTG2@users.noreply.github.com>
2023-03-03 08:43:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devil-Botz/Elsa.git /Elsa
+  git clone https://github.com/MAJORTG2/Christopher-Hemsworth.git /Christopher-Hemsworth
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Elsa
+  git clone $UPSTREAM_REPO /Christopher-Hemsworth
 fi
-cd /Elsa
+cd /Christopher-Hemsworth
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 Christopher-Hemsworth....🧞‍♂️"
 python3 bot.py
